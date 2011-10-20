534d826b0cd84d0fc2a5c7450083ca006abc0003
CodeFarmer <joel.gluth@gmail.com>
2011-10-20 13:24:07 +0100

    Buggy implementation of gain() needs fixing

---
+++ lib/decision_tree.rb
@@ -0,0 +1,85 @@
+module DecisionTree
+
+  # amap: map of (input, output)
+  # return: the entropy with regard to (output) of the current list
+  def self.entropy(amap)
+    output_counts = {}
+    amap.each do |i, o|
+      output_counts[o] ||= 0
+      output_counts[o] += 1
+    end
+
+    # it would be great if Ruby hashes had #reduce
+    e = 0
+    output_counts.each do |o, c|
+      p = c / amap.count
+      e += -p * Math.log2(p)
+    end
+
+    return e
+
+  end
+
+  # amap: map of (input, output) where input is also a map
+  # key:  a particular key from the input maps to use to partition the map
+  #
+  # return a new map, whose keys are the various values that 'key' takes in input, and whose values are the segments of map where input is equal that value (but with 'key' removed). This can include the nil key.
+  #
+  # Hmm. Makes sense when you say it out lout.
+  def self.partition(key, amap)
+
+    ret = {}
+
+    amap.each do |i, o|
+
+      part_without_key_in_keys = {}
+
+      amap.select do |k, v|
+        k[key] == i[key]
+      end.each do |k, v|
+        part_without_key_in_keys[k.select{|l, w| l != key}] = v
+      end
+
+      ret[i[key]] = part_without_key_in_keys
+
+    end
+
+    return ret
+
+  end
+
+  # alist: a list of maps
+  # return an Enumerable containing all of the keys that occur in the various maps
+  def self.all_keys(alist)
+    alist.reduce(Set.new) {|a, m| a.merge(m.keys)}.to_a
+  end
+
+  # amap: as with .partition, a map of (input, output) whose inputs are also maps
+  # return: the key which, when used to partition amap, produces the creates net information gain (across all the partitions) compared to amap
+  def self.most_informative_key(amap)
+
+    winner = nil
+    winning_gain = nil
+
+    current_entropy = entropy(amap)
+
+    all_keys(amap.keys).each do |key|
+      g = gain(key, amap, current_entropy)
+      if winner.nil? || g > winning_gain then
+        winner = key
+        winning_gain = g
+      end
+        
+    end
+
+    return winner
+
+  end
+
+  def self.gain(key, amap, current_entropy=nil)
+    current_entropy = entropy(amap) if current_entropy.nil?
+    return current_entropy - partition(key, amap).reduce(0) {|a, v| a + entropy(v)}
+  end
+
+end
+
---
+++ spec/lib/decision_tree_spec.rb
@@ -0,0 +1,146 @@
+require File.expand_path('../../../lib/decision_tree', __FILE__)
+
+describe DecisionTree do
+
+  describe '.entropy' do
+
+    it 'should return zero for an empty data set' do
+      DecisionTree.entropy({}).should == 0
+    end
+
+    it 'should return 0.940 for the example in the Mitchell book' do
+
+      nine_five = {
+        1 =>  'Y',
+        2 =>  'Y',
+        3 =>  'Y',
+        4 =>  'Y',
+        5 =>  'Y',
+        6 =>  'Y',
+        7 =>  'Y',
+        8 =>  'Y',
+        9 =>  'Y',
+        10 => 'N',
+        11 => 'N',
+        12 => 'N',
+        13 => 'N',
+        14 => 'N',
+      }
+
+      DecisionTree.entropy(nine_five).should be_within(0.0005).of(0.940)
+
+    end
+
+  end
+
+  describe '.partition' do
+
+    it 'should deal with the simple case of inputs that have matching keys' do
+
+      amap = {
+        {:a => 1, :b => 1} => 'A',
+        {:a => 2, :b => 1} => 'B',
+        {:a => 2, :b => 2} => 'C'
+      }
+
+      DecisionTree.partition(:a, amap).should == {
+        1 => {{:b => 1} => 'A'},
+        2 => {{:b => 1} => 'B', {:b => 2} => 'C'}
+      }
+
+    end
+
+    it 'should deal with inputs that have differing keys' do
+      amap = {
+        {:a => 1, :b => 1} => 'A',
+        {:a => 2, :b => 1} => 'B',
+        {:c => 2, :b => 2} => 'C',
+        {:d => 3, :b => 2} => 'D',
+      }
+
+      DecisionTree.partition(:a, amap).should == {
+        1   => {{:b => 1} => "A"},
+        2   => {{:b => 1} => "B"},
+        nil => {{:c => 2, :b =>2} =>"C", {:d => 3, :b => 2} => 'D'}
+      }
+      
+    end
+
+  end
+
+  describe '.all_keys' do
+    it 'should deal with the simple case of completely disjoint maps' do
+      DecisionTree.all_keys([{:a => 1, :b => 2}, {:c => 3, :d => 4}]).should == [:a, :b, :c, :d]
+    end
+    
+    it 'should return each key only once, when there are repeated keys' do
+      DecisionTree.all_keys([{:a => 1, :b => 2}, {:c => 3, :a => 4}]).should == [:a, :b, :c]
+    end
+  end
+
+  describe '.most_informative_key' do
+
+    it 'should return nil for an empty map' do
+      DecisionTree.most_informative_key({}).should be_nil
+    end
+
+    it 'should return a key when all are equally informative' do
+      amap = {
+        {:a => 1, :b => 2} => 'Y',
+        {:a => 2, :b => 3} => 'M'
+      }
+
+      mik = DecisionTree.most_informative_key(amap)
+      mik.should_not be_nil
+      mik.class.should == Symbol
+    end
+  end
+
+  describe 'with the example from the Mitchell book' do
+
+    before(:each) do
+      @sample_data = {
+        {:outlook => 'Sunny',    :temperature => 'Hot',  :humidity => 'High',   :wind => 'Weak'}   => 'No',
+        {:outlook => 'Sunny',    :temperature => 'Hot',  :humidity => 'High',   :wind => 'Strong'} => 'No',
+        {:outlook => 'Overcast', :temperature => 'Hot',  :humidity => 'High',   :wind => 'Strong'} => 'Yes',
+        {:outlook => 'Rain',     :temperature => 'Mild', :humidity => 'High',   :wind => 'Weak'}   => 'Yes',
+        {:outlook => 'Rain',     :temperature => 'Cool', :humidity => 'Normal', :wind => 'Weak'}   => 'Yes',
+        {:outlook => 'Rain',     :temperature => 'Cool', :humidity => 'Normal', :wind => 'Strong'} => 'No',
+        {:outlook => 'Overcast', :temperature => 'Cool', :humidity => 'Normal', :wind => 'Strong'} => 'Yes',
+        {:outlook => 'Sunny',    :temperature => 'Mild', :humidity => 'High',   :wind => 'Weak'}   => 'No',
+        {:outlook => 'Sunny',    :temperature => 'Cool', :humidity => 'Normal', :wind => 'Weak'}   => 'Yes',
+        {:outlook => 'Rain',     :temperature => 'Mild', :humidity => 'Normal', :wind => 'Weak'}   => 'Yes',
+        {:outlook => 'Sunny',    :temperature => 'Mild', :humidity => 'Normal', :wind => 'Strong'} => 'Yes',
+        {:outlook => 'Overcast', :temperature => 'Mild', :humidity => 'High',   :wind => 'Strong'} => 'Yes',
+        {:outlook => 'Overcast', :temperature => 'Hot',  :humidity => 'Normal', :wind => 'Weak'}   => 'Yes',
+        {:outlook => 'Rain',     :temperature => 'Mild', :humidity => 'High',   :wind => 'Strong'} => 'No',
+      }
+
+    end
+
+    describe '.gain' do
+      it 'should return 0.246 for :outlook' do
+        DecisionTree.gain(:outlook, @sample_data).should be_within(0.0005).of 0.246
+      end
+
+      it 'should return 0.151 for :humidity' do
+        DecisionTree.gain(:humidity, @sample_data).should be_within(0.0005).of 0.151
+      end
+
+      it 'should return 0.048 for :wind' do
+        DecisionTree.gain(:wind, @sample_data).should be_within(0.0005).of 0.048
+      end
+
+      it 'should return 0.029 for :temperature' do
+        DecisionTree.gain(:wind, @sample_data).should be_within(0.0005).of 0.048
+      end
+     end
+
+    describe '.most_informative_key' do
+      it 'should return :outlook as the first choice' do
+        DecisionTree.most_informative_key(@sample_data).should == :outlook
+      end
+    end
+  end
+
+end
